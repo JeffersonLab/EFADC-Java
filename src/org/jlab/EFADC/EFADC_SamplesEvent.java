2e1dc7874ccb7aacef89774691b479e9ba2c8576
John McKisson <mckisson@jlab.org>
2016-05-23 11:34:25 -0400

    Fix sign extensions and sample data indexing

--- src/org/jlab/EFADC/EFADC_SamplesEvent.java
+++ src/org/jlab/EFADC/EFADC_SamplesEvent.java
@@ -1,6 +1,10 @@
 package org.jlab.EFADC;
 
 import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+
+import java.nio.ByteBuffer;
+import java.util.logging.Logger;
 
 
 /**
@@ -10,6 +14,16 @@ import org.jboss.netty.buffer.ChannelBuffer;
  * Date: 11/13/12
  */
 public class EFADC_SamplesEvent extends AbstractEvent implements EFADC_Event {
+
+	public static final int IDX_COUNT = 0;
+	public static final int IDX_CHANNEL = 2;
+	public static final int IDX_TRIGID = 4;
+	public static final int IDX_TSTAMP = 6;
+	public static final int IDX_DATA = IDX_TSTAMP + 6;
+
+	public EFADC_SamplesEvent(ByteBuffer bytebuf) {
+		buf = ChannelBuffers.copiedBuffer(bytebuf);
+	}
 
 	public EFADC_SamplesEvent(int modId, ChannelBuffer frame) {
 		this.modId = (short)modId;
@@ -23,30 +37,29 @@ public class EFADC_SamplesEvent extends AbstractEvent implements EFADC_Event {
 		// or whatever, later
 		buf = frame;
 
-		// We need this stuff to be able to extract samples
-		//trigId = frame.readUnsignedShort();
-		//channel = frame.readUnsignedShort();
-		//sampleCount = (short)frame.readUnsignedShort();
-
 		return true;
 	}
 
 	public int getSampleCount() {
-		return buf.getShort(0);
+		return buf.getShort(IDX_COUNT) & 0x0000ffff;
 	}
 
 	public int getChannel() {
-		return buf.getShort(2);
+		return buf.getShort(IDX_CHANNEL) & 0x0000ffff;
 	}
 
 	public int getTriggerId() {
-		// Trigger ID is the first shortint in the buffer
-		return buf.getShort(4);
+		return buf.getShort(IDX_TRIGID) & 0x0000ffff;
 	}
 
 	@Override
 	public long getTimestamp() {
-		long tStamp = (buf.getInt(6) << 16) + buf.getShort(8);
+		long high = ((long)buf.getInt(IDX_TSTAMP) << 16) & 0x0000_ffff_ffff_0000L;
+		int low = (int)buf.getShort(IDX_TSTAMP + 4) & 0x0000_ffff;
+
+		long tStamp = (high | low) & 0x0000_ffff_ffff_ffffL;
+
+		//Logger.getLogger("global").info(String.format("high 0x%08x  low 0x%04x  tStamp 0x%08x", high, low, tStamp));
 
 		return tStamp;
 	}
@@ -77,7 +90,7 @@ public class EFADC_SamplesEvent extends AbstractEvent implements EFADC_Event {
 		//Extract sample data
 		for (int i = 0; i < count; i++) {
 
-			samples[i] = buf.getShort(6 + (i * 2));
+			samples[i] = buf.getShort(IDX_DATA + (i * 2)) & 0x0000ffff;
 
 		}
 
